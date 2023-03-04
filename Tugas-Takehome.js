d154ef4477156495542ffdb2a2b5cd0b22816604
Asepsa15 <asepsaripudin2002@gmail.com>
2023-03-05 00:38:08 +0700

    tugas-takehome

---
+++ Tugas-Takehome.js
@@ -0,0 +1,225 @@
+let var1;
+let var2;
+let var3;
+let var4;
+let var5;
+let var6;
+let var7;
+let var8;
+let var9;
+let var10;
+let var11;
+let var12;
+let var13;
+let var14;
+let var15;
+let var16;
+let var17;
+let var18;
+let var19;
+let var20;
+
+// Function var
+function setVar1(nama) {
+  var1 = nama;
+}
+
+function setVar2(tempatLahir) {
+  var2 = tempatLahir;
+}
+
+function setVar3(tanggalLahir) {
+  var3 = tanggalLahir;
+}
+
+function setVar4(jenisKelamin) {
+  var4 = jenisKelamin;
+}
+
+function setVar5(agama) {
+  var5 = agama;
+}
+
+function setVar6(kewarganegaraan) {
+  var6 = kewarganegaraan;
+}
+
+function setVar7(nim) {
+  var7 = nim;
+}
+
+function setVar8(alamat) {
+  var8 = alamat;
+}
+
+function setVar9(provinsi) {
+  var9 = provinsi;
+}
+
+function setVar10(kabupaten) {
+  var10 = kabupaten;
+}
+
+function setVar11(kecamatan) {
+  var11 = kecamatan;
+}
+
+function setVar12(desa) {
+  var12 = desa;
+}
+
+function setVar13(rt) {
+  var13 = rt;
+}
+
+function setVar14(rw) {
+  var14 = rw;
+}
+
+function setVar15(kodePos) {
+  var15 = kodePos;
+}
+
+function setVar16(jenisTinggal) {
+  var16 = jenisTinggal;
+}
+
+function setVar17(alatTranportasi) {
+  var17 = alatTranportasi;
+}
+
+function setVar18(universitas) {
+  var18 = universitas;
+}
+
+function setVar19(jurusan) {
+  var19 = jurusan;
+}
+
+function setVar20(falkultas) {
+  var20 = falkultas;
+}
+
+// Function get var
+function getVar1() {
+  return var1;
+}
+
+function getVar2() {
+  return var2;
+}
+
+function getVar3() {
+  return var3;
+}
+
+function getVar4() {
+  return var4;
+}
+function getVar5() {
+  return var5;
+}
+
+function getVar6() {
+  return var6;
+}
+
+function getVar7() {
+  return var7;
+}
+
+function getVar8() {
+  return var8;
+}
+function getVar9() {
+  return var9;
+}
+
+function getVar10() {
+  return var10;
+}
+
+function getVar11() {
+  return var11;
+}
+
+function getVar12() {
+  return var12;
+}
+
+function getVar13() {
+    return var13;
+}
+  
+function getVar14() {
+    return var14;
+}
+
+function getVar15() {
+    return var15;
+}
+  
+function getVar16() {
+    return var16;
+}
+  
+function getVar17() {
+    return var17;
+}
+  
+function getVar18() {
+    return var18;
+}
+
+function getVar19() {
+    return var19;
+}
+  
+function getVar20() {
+    return var20;
+}    
+
+
+// Pengisian Input
+setVar1("nama: Asep Saripudin");
+setVar2("Tempat Lahir : Sumedang");
+setVar3("Tanggal Lahir: 15-04-2002");
+setVar4("Jenis Kelamin : Laki Laki");
+setVar5("Agama : Islam");
+setVar6("Kewarganegaraan: Indonesia");
+setVar7("NIM : 10520727");
+setVar8("Alamat : Jl. Letda Lukito");
+setVar9("Provinsi : Jawa Barat");
+setVar10("Kabupaten: Kab.Sumedang");
+setVar11("Kecamatan : Jatinangor");
+setVar12("Desa: Cisempur");
+setVar13("RT : 01");
+setVar14("RW : 07");
+setVar15("Kode Pos : 45363");
+setVar16("Jenis Tinggal: Bersama Orang Tua");
+setVar17("Alat Tranportasi : Sepedah Motor");
+setVar18("Universitas: Universitas Komputer Indonesia");
+setVar19("Jurusan: Sisten Informasi");
+setVar20("Fakultas: Teknik & Ilmu Komputer");
+
+// Hasil Output
+console.log(getVar1()); 
+console.log(getVar2()); 
+console.log(getVar3()); 
+console.log(getVar4());
+console.log(getVar5()); 
+console.log(getVar6()); 
+console.log(getVar7()); 
+console.log(getVar8()); 
+console.log(getVar9()); 
+console.log(getVar10()); 
+console.log(getVar11()); 
+console.log(getVar12());
+console.log(getVar13()); 
+console.log(getVar14()); 
+console.log(getVar15()); 
+console.log(getVar16()); 
+console.log(getVar17()); 
+console.log(getVar18()); 
+console.log(getVar19()); 
+console.log(getVar20()); 
